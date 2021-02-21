ba5cb0c2c143ad3134feccc222b981a9d563b669
alperen2 <aalperendurmuss@gmail.com>
2021-02-21 23:28:11 +0300

    Dictionary component oluşturuldu

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css'
-import  PageHeader  from 'antd/lib/page-header';
+import PageHeader from 'antd/lib/page-header';
 import Navbar from "./components/Navbar"
+import Dictionary from "./components/Dictionary"
 import {
   BrowserRouter as Router,
+  Route
 } from "react-router-dom";
+
 
 
 function App() {
   return (
     <Router>
       <PageHeader title="Dictionary" subTitle="Learning dictionary" footer={<Navbar />} />
+      <Route path="dictionary" component={Dictionary} />
     </Router>
   )
 }
---
+++ src/components/Dictionary.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Link
+} from "react-router-dom";
+import Menu from 'antd/lib/menu';
+
+
+const Dictionary = () => {
+    return <h1>Hello world</h1>
+}
+
+export default Dictionary;
