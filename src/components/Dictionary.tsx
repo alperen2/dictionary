e628e90422ee202d8a82c98dc5187b9f982e1654
alperen2 <aalperendurmuss@gmail.com>
2021-02-28 13:31:39 +0300

    test

--- src/components/Dictionary.tsx
+++ src/components/Dictionary.tsx
@@ -3,8 +3,6 @@ import Search from 'antd/lib/input/Search';
 import Row from 'antd/lib/row';
 import React, { useEffect, useState } from 'react';
 import { Col } from 'antd/lib/grid';
-import Empty from 'antd/lib/empty';
-import { log } from 'console';
 
 interface API_CONFIG {
     app_id: string;
@@ -16,8 +14,6 @@ const API_CONFIG: API_CONFIG = {
     app_key: "d50dec262ee85fd0c4462fef816cf94b",
     language: "en-gb",
 }
-
-
 
 const Dictionary = () => {
     const [data, setData] = useState<any[]>([]);
@@ -35,21 +31,17 @@ const Dictionary = () => {
         if (value.length == 0) return false;
         setWord(value)
         setLoading(true)
-
         await fetch(`https://od-api.oxforddictionaries.com:443/api/v2/entries/${API_CONFIG.language}/${value.toLocaleLowerCase()}`, {
             headers: {
-                'app_id': 'd9059350',
-                'app_key': 'd50dec262ee85fd0c4462fef816cf94b',
-                'Access-Control-Allow-Origin': '*'
-
-            },
+                'app_id': API_CONFIG.app_id,
+                'app_key': API_CONFIG.app_key,
+            }
         })
-            .then(e => e.json())
-            .then(async result => {
-                console.log('====================================');
-                console.log(result);
-                console.log('====================================');
-            })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err));
+
+
     }
     return (
         <>
